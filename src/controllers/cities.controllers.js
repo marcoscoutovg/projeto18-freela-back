c8d7057f94cf2b61db4536ce89c4d5deff3a916a
Marcos Couto <marcoscouto1997@gmail.com>
2023-05-29 17:36:48 -0300

    backend projeto freela

--- src/controllers/cities.controllers.js
+++ src/controllers/cities.controllers.js
@@ -9,16 +9,3 @@ export async function getCities(req,res) {
         res.status(500).send(err.message);
     }
 }
-
-/* export async function cityById(req,res) {
-
-    const {id} = req.params
-
-    try {
-        const city = await db.query(`SELECT * FROM cities WHERE id = $1;`, [id])
-        res.send(city.rows[0])
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-}
-*/
