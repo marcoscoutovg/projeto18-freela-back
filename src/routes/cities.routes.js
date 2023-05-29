2f491049dffd6b35a21bc8375305c0221528dfd9
Marcos Couto <marcoscouto1997@gmail.com>
2023-05-29 17:27:29 -0300

    backend do projeto freela

--- src/routes/cities.routes.js
+++ src/routes/cities.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { cityById, getCities } from "../controllers/cities.controllers.js";
+import { getCities } from "../controllers/cities.controllers.js";
 
 const citiesRouter = Router()
 
